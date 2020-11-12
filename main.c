14f9e9e3c8d4b71f74204f81f283ca7dae0ab036
Christopher Lu <chlu@ruby.cecs.pdx.edu>
2020-11-12 11:16:44 -0800

    gutting

--- main.c
+++ main.c
@@ -4,21 +4,42 @@
 #define MAXARGS 15 //max command line args
 #define min(a, b) ((a) < (b) ? (a) :(b)) //if true a, else b
 
-struct argList
+struct argList //is this necessary?
 {
     int count;
-    char * args[MAXARGS];
+    char * args[MAXARGS]; //they're being put in separately
 } ArgList;
 
+typedef struct {
+    int count;
+    char *string;
+} funcParam; //for one or is this general
+
+//do we write one for each function?
+typedef void funcPtr(funcParam *); //what args go int?
+typedef struct {
+    char * description;
+    funcPtr * name;
+} dtEntry;
+
+void
+add(funcParam * input)
+{
+    printf("fuck you\n");
+    return;
+}
+
+dtEntry fdt[4] = 
+{
+    {"add", add}
+};
+int fdtCount = sizeof(fdt) / sizeof(fdt[0]);
+
 int printmenu(struct argList list); 
-int add();
-//virtual function array
 
 int main(int argc, char ** argv) //up to 15 args
 {
     int aCount = min(argc -1, MAXARGS);
-
-    printf("aCount is %d\n", aCount);
     if(argc == 1)
     {
         printf("too few gtfo \n");
@@ -28,27 +49,25 @@ int main(int argc, char ** argv) //up to 15 args
     int i;
     for(i = 0; i < aCount; i++)
     {
-        printf("%s\n", argv[i]);
         ArgList.args[i] = malloc(strlen(argv[i] + 1));
         strncpy(ArgList.args[i], argv[i], strlen(argv[i])+1);
     }
     ArgList.count = i;
 
-    //lol trying to print the args array
+    //
+
+
+    printmenu(ArgList);  //we could toss that into the fdt
+    return 0;
+}
+
+//currently just prints the struct
+int printmenu(struct argList l)
+{
+    printf("there are %d arguements \n", l.count);
     for(int i = 0; i < ArgList.count; i++) 
     {
         printf("arg[%d] == %s\n", i, ArgList.args[i]);    
     }
-    return 0;
-}
-
-int printmenu(struct argList l)
-{
-    printf("there are %d arguements \n", l.count);
-    return 0;
-}
-int add()
-{
-    printf("add\n");
     return 0;
 }
