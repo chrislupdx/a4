c223d7a52f5456dc645c24ea6ebd08dd96917afb
Christopher Lu <chlu@rita.cecs.pdx.edu>
2020-11-13 09:42:24 -0800

    exit currently works

--- main.c
+++ main.c
@@ -3,14 +3,13 @@
 #include <string.h>
 #define MAXARGS 15 //max command line args
 #define min(a, b) ((a) < (b) ? (a) :(b)) //if true a, else 
-#define funCount 1
-
+#define funCount 2
 typedef struct fParam  //lol what is this for
 {
     int count;
-    char *string;
+    char *string; 
+    char ** list; //2 dimensional array
 } funcParam; //for one or is this general
-
 typedef struct _menu
 {
     int count;
@@ -23,7 +22,6 @@ char * choices[] =
     "Multiplication", "Division", "Modulo",
     "Reverse Input"
 };
-
 typedef void funcPtr(funcParam *); //what args go int?
 typedef struct 
 {
@@ -37,13 +35,18 @@ void add(funcParam * input)
     return;
 }
 
+void eexit(funcParam * input)
+{
+    printf("goodbye\n");
+    return;
+}
 dtEntry fdt[4] = 
 {
-    {"add", add}
+    {"exit", eexit},
+    {"add", add},
 };
 int fdtCount = sizeof(fdt) / sizeof(fdt[0]);
 int printmenu(menu * menu); 
-
 int main(int argc, char ** argv) //up to 15 args
 {
     if(argc == 1)
@@ -51,20 +54,44 @@ int main(int argc, char ** argv) //up to 15 args
         printf("too few gtfo \n");
         return 0;
     }
-   
     menu menu;
     menu.count = funCount; 
-    //copy choices into menu
     for (int i = 0; i < menu.count; i++)
     {
         menu.items[i] = choices[i];
     }
     printmenu(&menu);
     
+    //Get userinput 
+    int ui;
+    printf("input: ");
+    int fuck = scanf("%i", &ui);
+    if(fuck < 0)
+    {
+        printf("sure\n");
+    }
 
+    //plug userinput into the fdt and call whatever is selected
+    printf("invoking %d: %s\n", ui, fdt[ui].description);
+
+
+    funcParam fp; //stuff the command line args into this 
+     
+    //parse hex
+    //parse int
+
+    //for(int i = 1; i < argc; i++)
+    //{
+    //    printf("%s\n", argv[i]);
+    //    int leng = strlen(argv[i]);
+    //    //fp.list[i] = malloc(leng + 1); //allocate the space for the arg
+    //    //strncpy(fp.list[i], argv[i], leng);
+    //    //fp.list[i] = argv[i];
+    //}
+
+    //FIGURE OUT HOW TO PROPOERTLY CONSUM 69-80 
     int funcMax = 1;  //this needs to be tweaked whenver we add fucntions into fdt
     funcParam * fList[funcMax]; 
-
     //call every function in the fdt
     for(int i = 0; i < funcMax; i++)
     {
@@ -74,8 +101,6 @@ int main(int argc, char ** argv) //up to 15 args
         (*fdt[i].name)(fList[i]); 
         free (fList[i]); 
     }
-
-
     return 0;
 }
 
@@ -83,11 +108,10 @@ int main(int argc, char ** argv) //up to 15 args
 int printmenu(menu * menu)
 {
     printf("\n");
-    for(int i = 0; i < menu->count; i++)
+    for(int i = 0; i < menu->count; i++) //menu count is currently set to 1 
     {
     printf("%d. %s\n", i, menu->items[i]);
     }
     printf("\n");
-
     return 0;
 }
