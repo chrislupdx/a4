e20aa9ad609b8472ce175109339ff6cb9d708677
Christopher Lu <chlu@rita.cecs.pdx.edu>
2020-11-16 14:46:03 -0800

    reverse works

--- main.c
+++ main.c
@@ -10,8 +10,6 @@ typedef struct fParam  //lol what is this for
 {
     int count;
     int * params; //this is a list of command line arguements cleand out for calculator consumption
-    //char * strings; //the argvs contatenated into a string
-    //int sLen;
 } funcParam; //for one or is this general
 
 //MENU
@@ -20,6 +18,7 @@ typedef struct _menu
     int count;
     char *items[funCount];
 } menu;
+
 char * choices[] = 
 {
     "Exit", "Addition", "Subtraction",
@@ -108,27 +107,17 @@ void modulo(funcParam fp)
 }
 
 //1. shove it into a struct :(( 2. put it into arguement
-//void reverse(funcParam fp, char * argvs) //not sure this will actually fly
 void reverse(int argc, char ** argv)
 {
-    //since the reverse is literal we can
-        //1. produce an alternate source
-        //2. 
-    //char print[fp.count];
-    //int counter = fp.count;
-    //printf("%d\n", counter);
-    //print out the command line args in reverse
-    //for(int i = (fp.count - 1); i >=0; i--)
-    //{
-    //    printf("%d\n", fp.params[i]);
-    //printf("\nsusan\n");
-    //for(int i = 0; i < fp.count; i++)
-    //{
-    //    //printf("i is %d\n", i);
-    //    printf("p is %d\n", print[i]);
-    //}
-
-    //copy  contents in reverse into print
+    for(int i = (argc - 1); i >= 1; i--)
+    {
+        int leng = strlen(argv[i]);
+        for(int x = leng; x >= 0; x--)
+        {
+        printf("%c", argv[i][x]);
+        }
+        printf(" ");
+    }
     return;
 }
 
@@ -162,14 +151,14 @@ int main(int argc, char ** argv) //up to 15 args
     //Process Command Line Argsa
     funcParam fp;
     int args = argc -1;
-   
+
     fp.params = (int*) malloc(argc * sizeof(args));
     int count = 0; 
     for(int i = 1; i < argc; i++) //starting the count here at 1
     {
         char * hexStr = argv[i];
         int base = 10; //if we need to tweak
-        
+
         if(hexStr[0] == '0')
         {
             if((hexStr[1] == 'x') || (hexStr[1] == 'X'))
@@ -183,36 +172,6 @@ int main(int argc, char ** argv) //up to 15 args
         //printf("params %d is %d \n", i, fp.params[i]);
     }
     fp.count = args;
-    
-    //Copy argv raw into a string format for reverse
-    //sum is the number of chars argv is alltogether
-    int sum = 0;
-    for(int i = 1; i < argc; i++)
-    {
-        int len = strlen(argv[i]); 
-        int newsum = len + sum; 
-        sum = newsum;
-    }
-    printf("sum of chars is %d\n", sum);
-   
-    //for the length of the empty array
-    //char copy[sum]; //the char length of the total 
-    //for(int i = 0; i < (sum - 1); i++) //is sum +1 over ?
-    //{
-        //char * arg = argv[i]; 
-        //printf("arg is %s\n", arg);
-        //int argLen = strlen(argv[i]);
-        //printf("str is %i\n", argLen); 
-
-        //go through each argv[i]
-        //for(int i = argLen; i > 0; i--)
-        //{
-        //    //printf("%c", arg[i]); 
-        //}
-        //copy[i] = current argv's current charset
-    //}
-    //copy each character into the char string argv by argv
-    //get the char len of all of the argvs or use a while loop
 
     //Generate the Menu
     menu menu;
@@ -239,13 +198,14 @@ int main(int argc, char ** argv) //up to 15 args
         printf("invoking %d: %s\n", ui, fdt[ui].description);
 
         //if erecerse, pass it 
-        if(ui == 7)
+        if(ui == 6)
         {
             (*fdt[ui].name)(argc, argv);
-        //go to reverse and implment reverse print
         }
-
-        (*fdt[ui].name)(fp);
+        else
+        {
+            (*fdt[ui].name)(fp);
+        }
 
         //if ui is 0 set done to 1
         if(ui == 0)
@@ -253,8 +213,6 @@ int main(int argc, char ** argv) //up to 15 args
             done = 1;
         }
     };
-
-    //remember to de-allocate/free() the char array in the struct
     return 0;
 }
 
